dbb036b9878cd373920495db627e190119abcee2
Ahmer Butt <ab15015@bristol.ac.uk>
2018-10-10 20:06:55 +0100

    lab1 3.4 test perceptron

--- ahmer/lab1/first_dnn.py
+++ ahmer/lab1/first_dnn.py
@@ -11,6 +11,7 @@ import os.path
 import numpy as np
 import pandas as pd
 from random import shuffle
+from functools import reduce
 
 def zip_with(xs, ys, f):
   return [ f(x, y) for (x, y) in zip(xs, ys) ]
@@ -68,25 +69,24 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 # run optimizer
 sess.run(tf.global_variables_initializer())
 
+def calc_accuracy(results, ground_truths, n):
+  # map results ([[0.1,0.66,0.34],...]) -> selected class ([1,...])
+  result_classes = np.fromiter((np.argmax(result) for result in results), dtype=float)
+
+  # return percentage of result_classes that matches ground_truths
+  f = lambda x, y: 1 if x == y else 0
+  corrects = zip_with(result_classes, ground_truths, f)
+  return reduce(lambda x, y: x + y, corrects) / n
+
 for epoch in range(10000):
   sess.run([optimizer], feed_dict={ x: train_x, y: train_y })
+  # 3.4 test your trained perceptron
+  # test performance every 100 epochs
   if epoch % 100 == 0:
     # results is an array [prob_of_1, prob_of_2, prob_of_3] with the results on test_x
-    results = sess.run(prediction, feed_dict={ x: test_x[:1], y: test_y[:1] }).tolist()
-    accuracy = calc_accuracy(results, train_y, 100)
+    results = sess.run(prediction, feed_dict={ x: test_x, y: test_y })
+    ground_truths = np.fromiter((np.argmax(y) for y in test_y.get_values()), dtype=float)
+    accuracy = calc_accuracy(results, ground_truths, 50) # 50 = len(results)
     print("Accuracy of Perceptron at epoch", epoch, "is", accuracy)
 
 print("===Training done===")
-
-def calc_accuracy(results, ground_truths, n):
-  findMaxIx = lambda result: np.argmax(result)
-  result_classes = np.vectorize(findMaxIx)(results)
-  f = lambda x, y: 1 if x == y else 0
-  return reduce(lambda x, y: x + y, zip_with(result_classes, ground_truths, f) / n
-
-
-# 3.4 test your tained perceptron
-# also add line to training to print accuracy at every 100 epochs
-
-sess.run(prediction, feed_dict={ x: test_x[:1], y: test_y[:1] }).toList()[0]
-
